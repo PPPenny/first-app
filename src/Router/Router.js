194e6d805894bc16f172b4db41dd765e9b05d97e
PPPenny <1020344938@qq.com>
2018-01-15 16:22:03 +0800

    修改router
    修改router

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,19 +1,27 @@
-/**
- * Created by Administrator on 2017/7/3.
- */
-import React,{Component}from 'react';
-import  {Router, Route, Link} from 'ReactRouterDOM';
-import {history} from 'history';
-import ShowTime from './show-time/show-time';
-const RouterBtn = ()=>{
-<Router history={history}>
-   <div>
-     <Switch>
-       <Route exact path="/ShowTime" component={ShowTime} />
-       <Route path="*" component={NotFound} />
-     </Switch>
-   </div>
- </Router>
-}
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Parent } from "../son-father/son-father";
+import ShowTime from "../show-time/show-time";
+const PrimaryLayout = () =>
+  <div className="primary-layout">
+    <header>测试</header>
+    <ul>
+      <li>
+        <Link to="/">Time</Link>
+      </li>
+      <li>
+        <Link to="/User">Parent</Link>
+      </li>
+    </ul>
+    <main>
+      <Route path="/" exact component={ShowTime} />
+      <Route path="/user" component={Parent} />
+    </main>
+  </div>
 
-// export default RouterBtn;
+const RouteDemo = () =>
+  <BrowserRouter>
+    <PrimaryLayout />
+  </BrowserRouter>;
+
+  export default RouteDemo;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import {Parent,Fathert} from './son-father/son-father';
 import Comptime from './component-time/component-time';
 import ShowTime from './show-time/show-time';
 import registerServiceWorker from './registerServiceWorker';
+import RouteDemo from './router/router';
 import './index.css';
-// import Router from './Router/Router';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Counter />, document.getElementById('test'));
@@ -16,6 +17,7 @@ ReactDOM.render(<Parent name="彭彭"/>, document.getElementById('test2'));
 ReactDOM.render(<Fathert/>, document.getElementById('test3'));
 ReactDOM.render(<Comptime/>, document.getElementById('test4'));
 ReactDOM.render(<ShowTime/>, document.getElementById('test5'));
-// ReactDOM.render(<Router/>, document.getElementById('test6'));
+ReactDOM.render(<RouteDemo/>, document.getElementById('test6'));
+
 
 registerServiceWorker();
